d9d7620b064aad548ae0683768e92787f71554f2
M. Hammad Yousuf <74192966+Hammad-Yousuf@users.noreply.github.com>
2024-07-02 11:05:59 +0500

    Selenium

--- Cambridge/testselenium.js
+++ Cambridge/testselenium.js
@@ -3,10 +3,10 @@ const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
 (async function example() {
   let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
   try {
-    await driver.get("https://www.google.com/ncr");
+    await driver.get("https://www.google.com/");
     await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
-    await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+    await driver.wait(until.titleIs("webdriver - Google Search"), 100000);
   } finally {
-    await driver.quit();
+    // await driver.quit();
   }
 })();
